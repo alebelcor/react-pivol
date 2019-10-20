bd44f1283c59498920e4cbfd61d7e3293733f7a3
alebelcor <856838+alebelcor@users.noreply.github.com>
2019-10-19 20:35:57 -0700

    Updates

---
+++ precache-manifest.067da18fa32b119a8f26dcae82e78546.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70264d571ccbf2c1595b4214c8543499",
+    "url": "/react-pivol/index.html"
+  },
+  {
+    "revision": "1984d04e16c602e4f2c6",
+    "url": "/react-pivol/static/css/2.bb342caa.chunk.css"
+  },
+  {
+    "revision": "1984d04e16c602e4f2c6",
+    "url": "/react-pivol/static/js/2.642773eb.chunk.js"
+  },
+  {
+    "revision": "de2aeff4a8c29e4f88ed",
+    "url": "/react-pivol/static/js/main.dfc0589c.chunk.js"
+  },
+  {
+    "revision": "901e77f66ec4555566e9",
+    "url": "/react-pivol/static/js/runtime-main.08fffc3d.js"
+  }
+]);
